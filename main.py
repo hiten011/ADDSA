b2afffaba38c49f7bd53869e7d28287f8dbdb3d3
hiten <hitengupta12345@gmail.com>
2025-08-02 20:11:00 +0930

    h

---
+++ main.py
@@ -0,0 +1,37 @@
+def add(num1, num2, base):
+    ans = 0
+    carry = 0
+    power = 1
+
+    while num1 > 0 or num2 > 0:
+        n1 = num1 % 10
+        n2 = num2 % 10
+
+        num1 //= 10
+        num2 //= 10
+
+        total = n1 + n2 + carry
+
+        carry = total // base
+        curSum = total % base
+
+        ans += curSum * power
+        power *= 10
+
+    ans += carry * power
+    return ans
+
+def multiply():
+    return 0
+
+def main():
+    num1 = int(input())
+    num2 = int(input())
+    base = int(input())
+
+    print(add(max(num1, num2), min(num1, num2), base))
+    print(multiply())
+    print(0)
+
+if __name__ == "__main__":
+    main()
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 main:
-	g++ -std=c++11 -o main.out -O2 -Wall main.cpp && ./main.out < main.txt
+	python3 main.py < main.txt
