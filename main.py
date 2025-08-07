8969febece509aad2922128fc394588542fb411e
hiten <hitengupta12345@gmail.com>
2025-08-07 22:51:50 +0930

    h

--- main.py
+++ main.py
@@ -40,26 +40,35 @@ def multiply(num1, num2):
     # Combine results
     return z2 * 10**(2 * m) + (z1 - z2 - z0) * 10**m + z0
 
-def convert(num, base):
+def to_base(num, base):
     ans = 0
-    carry = 0
     power = 1
 
     while num > 0:
-        n = num % 10
-        num //= 10
-        total = n + carry
+        n = num % base
+        num //= base
 
-        carry = total // base
-        curSum = total % base
+        curSum = n % base
 
         ans += curSum * power
         power *= 10
 
-    ans += carry * power
+    return ans
+
+def from_base(num, base):
+    ans = 0
+    idx = 0
+
+    while num > 0:
+        n = num % 10
+        num //= 10
+
+        ans += n * (base**idx)
+
+        idx += 1
+
     return ans
 
 s = input()
 num1, num2, base = map(int, s.split())
-
-print(add(max(num1, num2), min(num1, num2), base), convert(multiply(num1, num2), base), 0)
+print(add(max(num1, num2), min(num1, num2), base), to_base(multiply(from_base(num1, base), from_base(num2, base)), base), 0)
