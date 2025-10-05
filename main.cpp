23c701eeb522f12ed246d30e668b1bebcf61988d
hiten <hitengupta12345@gmail.com>
2025-10-06 10:01:59 +1030

    re

--- main.cpp
+++ main.cpp
@@ -1,112 +1,7 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// vector<string> getAdj(string &str) {
-//     str += ",";
-//     vector<string> vec;
-//     string prev = "";
-//     for (char c : str) {
-//         if (c == ',') {
-//             vec.push_back(prev);
-//             prev = "";
-//             continue;
-//         }
-
-//         prev += c;
-//     }
-
-//     return vec;
-// }
-
-// int getScore(char c) {
-//     if (isupper(c)) return (int) (c - 'A');
-//     return (int) (c - 'a') + 26;
-// }
-
-// int main() {
-//     string c, b, d;
-//     cin >> c >> b >> d;
-
-//     vector<string> country = getAdj(c);
-//     vector<string> build = getAdj(b);
-//     vector<string> destroy = getAdj(d);
-
-//     for (auto i : country) {
-//         for (auto j : i) cout << j << " ";
-//         cout << endl;
-//     }
-
-//     cout << endl;
-
-//     for (auto i : build)
-//     {
-//         for (auto j : i)
-//             cout << j << " ";
-//         cout << endl;
-//     }
-
-//     cout << endl;
-
-//     for (auto i : destroy)
-//     {
-//         for (auto j : i)
-//             cout << j << " ";
-//         cout << endl;
-//     }
-
-//     cout << endl;
-
-//     int n = destroy.size();
-//     int totalCost = 0;
-
-//     priority_queue<pair<int, pair<int, int>>> pq;
-
-//     // mst
-//     vector<bool> visited(n, false);
-//     pq.push({0, {-1, 0}});
-
-//     while (!pq.empty())
-//     {
-//         auto [cost, temp] = pq.top();
-//         auto [par, dest] = temp;
-
-//         pq.pop();
-
-//         if (cost < 0) {
-//             // build road
-//             if (visited[dest]) continue; // already visited
-
-//             totalCost += abs(cost);
-//         } else if (visited[dest]) {
-//             totalCost += cost;
-//             continue;
-//         }
-
-//         visited[dest] = true;
-//         cout << dest << endl;
-//         for (int i = 0; i < n; i++) {
-//             if (i == par || i == dest) continue;
-
-//             if (country[dest][i] == '1') {
-//                 // edge exists
-//                 // cout << getScore(destroy[dest][i]) << " " << i << endl;
-//                 pq.push({getScore(destroy[dest][i]), {dest, i}});
-//             } else {
-//                 // build edge
-//                 pq.push({-1 * getScore(build[dest][i]), {dest, i}});
-//             }
-//         }
-//     }
-
-//     cout << totalCost << endl;
-    
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> getAdj(string &str)
-{
+vector<string> getAdj(string &str) {
     str += ",";
     vector<string> vec;
     string prev = "";
@@ -125,15 +20,12 @@ vector<string> getAdj(string &str)
     return vec;
 }
 
-int getScore(char c)
-{
-    if (isupper(c))
-        return (int)(c - 'A');
+int getScore(char c) {
+    if (isupper(c)) return (int)(c - 'A');
     return (int)(c - 'a') + 26;
 }
 
-int main()
-{
+int main() {
     string c, b, d;
     cin >> c >> b >> d;
 
@@ -141,48 +33,19 @@ int main()
     vector<string> build = getAdj(b);
     vector<string> destroy = getAdj(d);
 
-    // for (auto i : country)
-    // {
-    //     for (auto j : i)
-    //         cout << j << " ";
-    //     cout << endl;
-    // }
-
-    // cout << endl;
-
-    // for (auto i : build)
-    // {
-    //     for (auto j : i)
-    //         cout << j << " ";
-    //     cout << endl;
-    // }
-
-    // cout << endl;
-
-    // for (auto i : destroy)
-    // {
-    //     for (auto j : i)
-    //         cout << j << " ";
-    //     cout << endl;
-    // }
-
-    // cout << endl;
-
     int n = destroy.size();
     int totalCost = 0;
 
-    priority_queue<pair<int, pair<int, int>>> pq;
+    priority_queue<pair<int, int>> pq;
 
     // mst
     vector<bool> visited(n, false);
-    pq.push({0, {-1, 0}});
+    pq.push({0, 0});
 
     while (!pq.empty()) {
-        auto top = pq.top();    
+        pair<int, int> top = pq.top();    
         int cost = top.first;   
-        auto temp = top.second; 
-        int par = temp.first;   
-        int dest = temp.second;
+        int dest = top.second;
 
         pq.pop();
 
@@ -199,32 +62,22 @@ int main()
         }
 
         visited[dest] = true;
-        // cout << dest << endl;
         for (int i = 0; i < n; i++) {
             if (i == dest || country[dest][i] == '2') continue;
 
             if (country[dest][i] == '1') {
                 // edge exists
-                // cout << getScore(destroy[dest][i]) << " " << i << endl;
-                pq.push({getScore(destroy[dest][i]), {dest, i}});
+                pq.push({getScore(destroy[dest][i]), i});
             }
-            else if (country[dest][i] == '0')
-            {
+            else if (country[dest][i] == '0') {
                 // build edge
-                pq.push({-1 * getScore(build[dest][i]), {dest, i}});
+                pq.push({-1 * getScore(build[dest][i]), i});
             }
 
             country[i][dest] = '2'; // process road
             country[dest][i] = '2';   // process road
         }
     }
-
-    // for (auto i : country)
-    // {
-    //     for (auto j : i)
-    //         cout << j << " ";
-    //     cout << endl;
-    // }
 
     cout << totalCost << endl;
 }
